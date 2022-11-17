868cbbfbf2f2b9f5c360cb98d67b9896a90a0f39
malik masis <malik.masis@gmail.com>
2022-11-17 14:02:15 +0300

    Injecting DaprClient

---
+++ src/DaprExample.Application/DaprClientInjectingService.cs
@@ -0,0 +1,27 @@
+﻿using Dapr.Client;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace DaprExample
+{
+    public class DaprClientInjectingService : ITransientDependency
+    {
+        private readonly DaprClient _daprClient;
+
+        public DaprClientInjectingService(DaprClient daprClient)
+        {
+            _daprClient = daprClient;
+        }
+
+        public async Task DoItAsync()
+        {
+            await _daprClient.PublishEventAsync("pubsub", "newOrder", new Order { Id = 1, Amount = 100 });
+        }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+    }
+}
