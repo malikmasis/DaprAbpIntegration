cb4b20302cf76390488caaa9aa399a1e052b891b
malik masis <malik.masis@gmail.com>
2022-11-17 14:02:24 +0300

    IAbpDaprClientFactory

---
+++ src/DaprExample.Application/AbpDaprClientFactoryService.cs
@@ -0,0 +1,37 @@
+﻿using Dapr.Client;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Volo.Abp.Dapr;
+using Volo.Abp.DependencyInjection;
+
+namespace DaprExample
+{
+    public class AbpDaprClientFactoryService : ITransientDependency
+    {
+        private readonly IAbpDaprClientFactory _daprClientFactory;
+
+        public AbpDaprClientFactoryService(IAbpDaprClientFactory daprClientFactory)
+        {
+            _daprClientFactory = daprClientFactory;
+        }
+
+        public async Task DoItAsync()
+        {
+            //TODO Create or CreateAsync ??
+            // Create a DaprClient object with default options
+            DaprClient daprClient =  _daprClientFactory.Create();
+            
+            /* Create a DaprClient object with configuring
+             * the DaprClientBuilder object */
+            DaprClient daprClient2 =  _daprClientFactory
+                .Create(builder =>
+                {
+                    builder.UseDaprApiToken("...");
+                });
+
+            // Create an HttpClient object
+            HttpClient httpClient =  _daprClientFactory
+                .CreateHttpClient("target-app-id");
+        }
+    }
+}
