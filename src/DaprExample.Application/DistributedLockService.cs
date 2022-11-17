7f2e4fcb2041544415774cef0aa2409295ae187f
malik masis <malik.masis@gmail.com>
2022-11-17 14:03:34 +0300

    Distributed Lock

---
+++ src/DaprExample.Application/DistributedLockService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.DistributedLocking;
+
+namespace DaprExample
+{
+    public class DistributedLockService : ITransientDependency
+    {
+        private readonly IAbpDistributedLock _distributedLock;
+
+        public DistributedLockService(IAbpDistributedLock distributedLock)
+        {
+            _distributedLock = distributedLock;
+        }
+
+        public async Task MyMethodAsync()
+        {
+            await using var handle =
+                         await _distributedLock.TryAcquireAsync("MyLockName");
+            if (handle != null)
+            {
+                // your code that access the shared resource
+            }
+        }
+    }
+}
