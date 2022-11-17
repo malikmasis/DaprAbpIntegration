9fb68b0c276628a3e4d940a236a9fbf009386bd3
malik masis <malik.masis@gmail.com>
2022-11-17 14:03:50 +0300

    Security

---
+++ src/DaprExample.Application/TestRouteController.cs
@@ -0,0 +1,27 @@
+﻿using Dapr;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.Dapr;
+
+namespace DaprExample
+{
+    public class MyController : AbpController
+    {
+        [HttpPost("/stock-changed")]
+        [Topic("pubsub", "StockChanged")]
+        public async Task<IActionResult> TestRouteAsync([FromBody] StockCountChangedEto model)
+        {
+            // Validate the App API token!
+            HttpContext.ValidateDaprAppApiToken();
+
+            // Do something with the event
+            return Ok();
+        }
+    }
+}
